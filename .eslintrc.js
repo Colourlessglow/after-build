693d5b5e8d2fe1adafd2f9e80ca441d90b7904aa
xuxinjie <1075790909@qq.com>
2022-05-04 16:48:20 +0800

    修改：项目代码规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,8 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
   parserOptions: {
     ecmaVersion: 2020,
   },
-  plugins: ['prettier'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  extends: ['@vill-v'],
 }
